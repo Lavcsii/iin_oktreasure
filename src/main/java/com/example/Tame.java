02923c0e6551b05142ed85ec70a95c5286fe0061
Fazekas Laura <fazekaslauraalexandra2002@gmail>
2026-02-23 21:07:24 +0100

    Refaktorálás elkészült

--- src/main/java/com/example/Tame.java
+++ src/main/java/com/example/Tame.java
@@ -1,9 +1,9 @@
 /*
 * File: Tame.java
-* Author: Sallai András
-* Copyright: 2026, Sallai András
-* Group: szit.hu
-* Date: 2026-02-15
+* Author: Fazekas Laura
+* Copyright: 2026, Fazekas Laura
+* Group: SZOFT-IIN
+* Date: 2026-02-23
 * Github: https://github.com/oktatrefakt/
 * Licenc: MIT
 */
@@ -17,26 +17,28 @@ import java.time.LocalDate;
 
 /* Az osztály neve: Tame */
 public class Tame {
-    /* Ez egy fájlkezelő osztály. Úgy lett megírva, hogy
-    elfogadja a paraméterként egy szöveg. Potosabban a
-    benne lévő csa metóuds fogadja a paramétert.
-    Jelenleg nincs ellenőrzés, hogy megfelel-e a szöveg.
-    Talán nem is lesz, ez még a jövő titka. */
+    /*
+     * Ez egy fájlkezelő osztály. Úgy lett megírva, hogy
+     * elfogadja a paraméterként egy szöveg. Potosabban a
+     * benne lévő csa metóuds fogadja a paramétert.
+     * Jelenleg nincs ellenőrzés, hogy megfelel-e a szöveg.
+     * Talán nem is lesz, ez még a jövő titka.
+     */
     public static void csa(String d) {
-        //Itt fogadjuk a paramétert a d változóban
+        // Itt fogadjuk a paramétert a d változóban
         try {
             FileWriter fileWriter = new FileWriter("adat.txt", Charset.forName("utf-8"), true);
-            //Kettőspontot írunk a szövegben szeparátornak
+            // Kettőspontot írunk a szövegben szeparátornak
             fileWriter.write(d + ":");
-            //Dátumot is kiírunk
+            // Dátumot is kiírunk
             LocalDate now = LocalDate.now();
             fileWriter.write(now.toString() + "\n");
-            //Bezárjuk a fájl. Ezt mindig kell.
+            // Bezárjuk a fájl. Ezt mindig kell.
             fileWriter.close();
-        } catch (IOException e) {  // catch ág kezdete
-            //A catch ág hiba esetén
+        } catch (IOException e) { // catch ág kezdete
+            // A catch ág hiba esetén
             System.err.println(e.getMessage());
-        }//catch vége
-        
+        } // catch vége
+
     }
 }
